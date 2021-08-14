709d42633aeeb806949bc89346a9ff36de92c1e8
Sudhanshu Dubey <sudhanshu.thethunder@gmail.com>
2021-08-15 02:35:18 +0530

    [Code] AccountBalanceUI app created from Login template using Android Studio

---
+++ src/modern_UI/AccountBalanceUI/app/src/main/java/com/example/accountbalanceui/data/model/LoggedInUser.java
@@ -0,0 +1,23 @@
+package com.example.accountbalanceui.data.model;
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+public class LoggedInUser {
+
+    private final String userId;
+    private final String displayName;
+
+    public LoggedInUser(String userId, String displayName) {
+        this.userId = userId;
+        this.displayName = displayName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
